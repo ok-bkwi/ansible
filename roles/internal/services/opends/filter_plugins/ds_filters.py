3b8054435d6d170ff22cf5e101e14f47ca9d1c8f
onknows <onno@onknows.com>
2021-01-22 12:32:21 +0100

    opends filter opends_cmd

--- roles/internal/services/opends/filter_plugins/ds_filters.py
+++ roles/internal/services/opends/filter_plugins/ds_filters.py
@@ -4,16 +4,13 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible.errors import AnsibleFilterError
-import json, os
 
-# Return a logical name based on inventory file
-# e.g. development (from /somepath/development.ini)
+# Return command switches for OpenDS
 def opends_cmd(cmd_config):
-   # bn = os.path.basename(inv_file)
-   # return bn.split('.',1)[0]
-   # d = {'x': 1, 'y': 2, 'z': 3}
-   # test = ''.join(x for x in d.values())
-   return ''
+  cmd_line = ''
+  for key in cmd_config:
+    cmd_line += ' --{} {}'.format(key,cmd_config[key])
+  return cmd_line
 
 class FilterModule(object):
     """ansible filters."""
